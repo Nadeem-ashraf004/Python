3bb56522177e53c2bdb924b2d840ae9c02adcb8a
Nadeem-ashraf004 <nadeemmsaqi@gmail.com.com>
2025-01-01 11:11:09 +0500

    file add

---
+++ string.py
@@ -0,0 +1,12 @@
+#slicing the string
+b="hello, world"
+print(b[:5])
+#negative slicing
+b="Hello world "
+print(b[-2:-5])
+#upper case method 
+b=" Hello World"
+print (b.upper())
+print(b.lower())
+print (b.replace("H","K"))
+print(b.strip())
