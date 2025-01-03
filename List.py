d0f1088a62fe0861f31b0a72e6e0b8aa4740d382
Nadeem-ashraf004 <nadeemmsaqi@gmail.com.com>
2025-01-03 21:57:05 +0500

    list file

---
+++ List.py
@@ -0,0 +1,30 @@
+a=[12,13,14]
+print(a[2])
+a.append(13)
+a.remove(12)
+print(a)
+########################## creating a list
+a=[1,2,3,4,5]
+b=["apple","banana","orange"]
+c=[1,"nadeem",'A',2.3,True]
+print(a)
+print(b)
+print(c)
+############################using list contructor
+l=[13,"apple",1,1.2]
+print(l)
+############### create the by repeated element
+w=[9]*7
+r=[0]*10
+print(w)
+print(r)
+################ adding the into the list
+y=[]
+y.append(13)
+print("after append  :",y)
+y.insert(0,1)
+print("after insert  :",y)
+y.extend([4,56,])
+print("after extend :",y)
+lenght=len(y)
+print(lenght)
--- operator.py
+++ operator.py
@@ -5,5 +5,11 @@ z=x
 print(x is z)
 
 print(x is y)
+#
+n=[2,2,4,6]
+p=[1,2,34,55]
+x=n
 
+print(x is not n)
+print(n is not p)
 
