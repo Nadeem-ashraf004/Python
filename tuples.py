089f84ab42d6f1694e31cdbc11930fa66913bb11
Nadeem-ashraf004 <nadeemmsaqi@gmail.com.com>
2025-01-04 23:12:50 +0500

    tuple file

---
+++ tuples.py
@@ -0,0 +1,15 @@
+tup=(10)
+print(tup)
+## using string
+tup =('greet',"for")
+
+##using list
+li=[1,2,3,4,5]
+print(tuple(li))
+### using build in function
+tup=tuple('greet')
+print(tup)
+## 
+a=(1,2,3)
+b=(4,5)
+print(a*2+b)
