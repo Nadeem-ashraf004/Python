32a8ffa24f307fb4a1d8fdb5149f1de0232e5c1c
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-01-12 11:28:17 +0500

    dictionary

---
+++ Dictionary.py
@@ -0,0 +1,32 @@
+d={1:"nadeem",2:"ashraf", 3:"ali"}
+print(d)
+x=dict(a="nadeem",b="ashraf", c="ali")
+del x["c"]
+print(x)
+val=x.pop('b')
+print(val)
+key ,val = x.popitem()
+print(f"key :{key},value:{val}")
+print(x.get("a"))
+x.clear()
+print(x)
+##############################################################
+d={
+    "product":{"name":"laptop","price":45000,"stock":40},
+    "product2":{"name":"mobilephones","price":4000,"stock":20},
+    "product3":{"name":"tablet","price ":499,"stock":10}
+}
+cnt=0
+for key ,val in d.items():
+    if isinstance(val,dict):
+        for i in val:
+            cnt+=1
+    else:
+        cnt+=1
+print(cnt) 
+lenght1=len(d.keys())
+print(lenght1)
+lenght2=len(d.values())
+print(lenght2)
+lenght3=len(d.items())
+print(lenght3)      
--- temperature.py
+++ temperature.py
@@ -1,3 +1,7 @@
-celsius=37.5
-fahrenheit=(celsius*1.8)+32
-print("%0. if degree celsuis is equel to %0.if degree Fahrenheit"%fahrenheit(celsius))
+celsius = 37.5
+fahrenheit = (celsius * 1.8) + 32
+print("%.1f degrees Celsius is equal to %.1f degrees Fahrenheit" % (celsius, fahrenheit))
+#######
+celcius=int(input("enter your currrent celcius : "))
+fahrenheit = (celsius * 1.8) + 32
+print("%.f degree of celcius is equal to %.f degree Fahrenheit" %(celcius,fahrenheit))
