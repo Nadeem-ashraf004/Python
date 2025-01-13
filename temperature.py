d0dd34b5832783d3377699b9f4583b964bb45da9
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-01-13 17:16:02 +0500

    calculate the area of the triangle

--- temperature.py
+++ temperature.py
@@ -5,3 +5,12 @@ print("%.1f degrees Celsius is equal to %.1f degrees Fahrenheit" % (celsius, fah
 celcius=int(input("enter your currrent celcius : "))
 fahrenheit = (celsius * 1.8) + 32
 print("%.f degree of celcius is equal to %.f degree Fahrenheit" %(celcius,fahrenheit))
+
+
+#### calculate the area of the triangle
+x=float(input("Enter your first lenght side :"))
+y=float(input("Enter your secode side:"))
+z=float(input("Enter your third side :"))
+s=(x+y+z)/2
+area=(s*(s-x)*(s-y)*(s-z))**0.5
+print("the area of the triangle is %0.2f"%area)
