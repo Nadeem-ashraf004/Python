90831faef1078b3eadec57c09a9391c18cf40ad5
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-01-14 23:29:49 +0500

    function

---
+++ function.py
@@ -0,0 +1,15 @@
+def add(num1:int,num2:int) ->int:
+    num3=num1 + num2
+    return num3
+num1=int(input("Enter your first number :"))
+num2=int(input("Enter your second number :"))
+ans=add(num1,num2)
+print(f"the addition of {num1} and {num2} is equal to {ans}.")
+################ multipication of two number
+def mult(num1:int,num2:int)->int:
+    num3=num1*num2
+    return num3
+num1=int(input("Enter your first number :"))
+num2=int(input("Enter your seconde number :"))
+ans=mult(num1,num2)
+print(f"th multiplication of {num1} and {num2} is equal to {ans}")
