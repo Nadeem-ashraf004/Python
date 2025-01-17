d630dbd032fa7a2374cd69be54b2e00655981961
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-01-17 21:56:28 +0500

    file

--- function.py
+++ function.py
@@ -19,3 +19,9 @@ def fun (x,y=50):
     print('y =',y)
 fun(39)
 #example2
+import math
+def calculate_circle_area(radius):
+    return math.pi * radius ** 2
+radius = float(input("Enter the radius of the circle: "))
+area = calculate_circle_area(radius)
+print(f"The area of the circle with radius {radius} is {area:.2f}")
