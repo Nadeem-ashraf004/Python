eb9da16efdddf35c97e28cfe65da1b95f6565534
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-01-19 20:57:17 +0500

    add

--- function.py
+++ function.py
@@ -23,5 +23,14 @@ import math
 def calculate_circle_area(radius):
     return math.pi * radius ** 2
 radius = float(input("Enter the radius of the circle: "))
+
+area = calculate_circle_area(radius)
+print(f"The area of the circle with radius {radius} is {area:.2f}")
+############################
+import math
+def calculate_circle_area(radius):
+    return math.pi * radius ** 2
+radius = float(input("Enter the radius of the circle: "))
+
 area = calculate_circle_area(radius)
 print(f"The area of the circle with radius {radius} is {area:.2f}")
