007c027e30aaf029927cb099492968cd6badda66
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-01-20 19:54:15 +0500

    prime number

--- function.py
+++ function.py
@@ -27,10 +27,19 @@ radius = float(input("Enter the radius of the circle: "))
 area = calculate_circle_area(radius)
 print(f"The area of the circle with radius {radius} is {area:.2f}")
 ############################
-import math
-def calculate_circle_area(radius):
-    return math.pi * radius ** 2
-radius = float(input("Enter the radius of the circle: "))
+def fun(n):
+    x = 2
+    count = 0
+    while count < n:
+        for d in range(2, int(x ** 0.5) + 1):  # check divisibility only up to sqrt(x)
+            if x % d == 0:
+                break  # if divisible, it's not prime, so break the loop
+        else:
+            print(x)  # prime number
+            count += 1
+        x += 1
 
-area = calculate_circle_area(radius)
-print(f"The area of the circle with radius {radius} is {area:.2f}")
+# Driver Code
+n = 10
+
+fun(n)
