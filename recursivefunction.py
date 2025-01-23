a7880362348b918d122509dc4a62a5a09c7db029
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-01-23 17:45:43 +0500

    commited to learn

--- recursivefunction.py
+++ recursivefunction.py
@@ -3,4 +3,14 @@ def factorial(n):
         return 1
     else :
         return n*factorial(n-1)
-print (factorial(5))    
+print (factorial(5))   
+######################
+def febbonacci(n):
+    if n==0:
+        return 0
+    elif n== 1:
+        return 1
+    else:
+        return febbonacci(n-1) + febbonacci(n-2)
+print(febbonacci(10))    
+    
