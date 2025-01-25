99c3fe6a4f9d4eeead86ae2dcb0a8316a7c66518
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-01-25 21:45:12 +0500

    add

--- recursivefunction.py
+++ recursivefunction.py
@@ -14,8 +14,9 @@ def febbonacci(n):
         return febbonacci(n-1) + febbonacci(n-2)
 print(febbonacci(10))    
     ########################
-# def revers_string(s):
-#     if len(s)==0:
-#         return 0
-#     return revers_string(s[:1])+s[0] 
-# print(revers_string("hello"))  
+def reverse(s):
+    if len(s) == 0:
+        return s
+    return reverse(s[1:])+s[0] 
+print(reverse("hello"))  
+
