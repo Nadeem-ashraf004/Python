31e857b07da4743846f6290045c25d317ddf3038
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-01-30 12:44:05 +0500

    instance

--- selfargument.py
+++ selfargument.py
@@ -4,8 +4,10 @@ class Car:
         self.brand=brand
     def display(self):
         return self.brand,self.model
-Car1=Car("toyota","Corola") 
-print(Car1.display())
+Car1=input("Enter you  car model :\n")
+car2=input("Enter your brand :") 
+Car3=Car(Car1,car2)
+print(Car3.display())
 ###########################
 class Circle:
     def __init__(self,r):
@@ -13,5 +15,6 @@ class Circle:
     def area(self):
         a=3.14*self.r**2  
         return a
-ins=Circle(5)   
+radius=float(input("Enter your area of circle :"))    
+ins=Circle(radius)   
 print("Area of the circle : ",ins.area())   
