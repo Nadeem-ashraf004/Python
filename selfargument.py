b0299c89b396dd8ef52037879ff97d43ffa4f36d
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-01-30 11:58:35 +0500

    add file

---
+++ selfargument.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self,brand,model):
+        self.model=model
+        self.brand=brand
+    def display(self):
+        return self.brand,self.model
+Car1=Car("toyota","Corola") 
+print(Car1.display())      
+############################
