1d45a8b1eb642cb237426295848b5ed8625dd9fd
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-03 20:57:29 +0500

    add

--- OOPs_python/classandobject.py
+++ OOPs_python/classandobject.py
@@ -11,3 +11,15 @@ class Dog():
 dog1=Dog("gundda",3)  
 print(dog1.name)
 print(dog1.species)
+#################
+class Dog():
+    species="canine"
+    def __init__(self,name,age):
+           self.name=name
+           self.age=age
+    def __str__(self):
+        return f"{self.name} is {self.age} year old !"
+dog1=Dog("charr",3)
+dog2=Dog("Vhar",5)
+print(dog1)
+print(dog2)           
