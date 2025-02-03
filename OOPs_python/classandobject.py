64e294cd118fe032a216edc706642aab407349bb
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-03 20:48:38 +0500

    a

--- OOPs_python/classandobject.py
+++ OOPs_python/classandobject.py
@@ -3,3 +3,11 @@ class Dog():
 dog1=Dog() ##instance object to directly acccess the class attributs
 print(dog1.sound)    
 ##############################
+class Dog():
+    species="Canine"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+dog1=Dog("gundda",3)  
+print(dog1.name)
+print(dog1.species)
