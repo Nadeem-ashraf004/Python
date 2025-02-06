cb985afe659cba613e3ecd3aa7ad5039352e73b5
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-06 17:17:44 +0500

    A

--- enumeratfunc.py
+++ enumeratfunc.py
@@ -1,3 +1,6 @@
 for key,value in enumerate(['ali','gulzar','khan']):
     print(key,value)
-    
+###########################
+for key,value in enumerate(['I','Am','A','very','good','Programer']):
+    print(value,end=" ")    
+
