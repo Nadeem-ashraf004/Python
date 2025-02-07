827080b4bed0fd41fb395141994d879e7de31f59
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-07 10:33:33 +0500

    A

---
+++ OOPs_python/inheritanc.py
@@ -0,0 +1,8 @@
+class person():
+    def __init__(self,fname,lname):
+        self.fname=fname
+        self.lname=lname
+    def name(self):
+        print(self.fname,self.lname) 
+x=person('Nadeem','ashraf')
+x.name()           
