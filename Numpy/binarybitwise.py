acb70e6f0132dcc706e668cc5892b309b3c0ce54
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-09 22:11:35 +0500

    A

---
+++ Numpy/binarybitwise.py
@@ -0,0 +1,7 @@
+import numpy as np
+a=10
+b=12
+print("the first number you enter :")
+print("the seconde number  you enter : ")
+num_bits=np.bitwise_and(a,b)
+print("the bitwise number is = ",num_bits)
