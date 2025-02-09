0b8533259c452666f3a3f3770aaea84fce84a15a
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-09 21:57:59 +0500

    A

--- Numpy/methodsarray.py
+++ Numpy/methodsarray.py
@@ -8,3 +8,8 @@ print("basic sequenc :",sequence)
 ###### only print even number 
 evennumber=np.arange(2,20,2)
 print(evennumber)
+########
+number=np.arange(0,20,3)
+filter=number[number>10]
+print("filtered number :",filter)
+###########
