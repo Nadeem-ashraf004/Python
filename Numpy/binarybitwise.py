6b3f2f21d77821a10689e51ae086b8241ad326f4
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-09 22:24:42 +0500

    A

--- Numpy/binarybitwise.py
+++ Numpy/binarybitwise.py
@@ -1,8 +1,11 @@
 import numpy as np
+######## bitwise_and
 a=10
 b=12
 print("the first number you enter :")
 print("the seconde number  you enter : ")
 num_bits=np.bitwise_and(a,b)
 print("the bitwise number is = ",num_bits)
-#############################
+############################# bitwise_or
+num1_bit=np.bitwise_or(a,b)
+print("if we just or bitwise function here is the result : ",num1_bit)
