34a967eaf314ee2a23cd354b8aac2af01d274f15
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-10 13:04:06 +0500

    A

--- Numpy/binarybitwise.py
+++ Numpy/binarybitwise.py
@@ -9,3 +9,7 @@ print("the bitwise number is = ",num_bits)
 ############################# bitwise_or
 num1_bit=np.bitwise_or(a,b)
 print("if we just or bitwise function here is the result : ",num1_bit)
+####################bitwise xor
+num1_bit=np.bitwise_xor(a,b)
+print("XOR operation is done :",num1_bit)
+
