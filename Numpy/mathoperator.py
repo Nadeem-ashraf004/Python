6df8a0e739526f82d50959980c653f7d28e39b80
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-11 11:19:44 +0500

    A

---
+++ Numpy/mathoperator.py
@@ -0,0 +1,5 @@
+############################# using trignomatic function
+import numpy as np
+array=[0,math.pi/2,np.pi/3,np.pi/4]
+sin_value=np.sin(array)
+print("after the sin function :",sin_value)
