d36c2d5c700ea460947aa849435c000a408f8200
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-13 10:34:08 +0500

    line graph

--- matplotlib/linegraph.py
+++ matplotlib/linegraph.py
@@ -14,5 +14,10 @@ plt.title("Line Graph ")
 plt.legend(["Line"])
 plt.show()
 ########################
-
+x=[1,2,3,4,5]
+y=[10,20,15,25,30]
+plt.plot(x,y,marker='o',linestyle='-',color='b',label='data line')
+plt.legend()
+plt.title("Basic Line Graph ")
+plt.show()
  
