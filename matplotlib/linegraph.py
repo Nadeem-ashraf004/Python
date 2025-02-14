41acea77ce77bd82eeff2c32a28492523d0a5ce6
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-14 10:24:07 +0500

    multiline graph

--- matplotlib/linegraph.py
+++ matplotlib/linegraph.py
@@ -21,5 +21,15 @@ plt.legend()
 plt.title("Basic Line Graph ")
 plt.show()
 #########################
-
- 
+x1=np.array([1,2,3,4,5])
+y1=x1*2
+plt.plot(x,y)
+x2=[2,4,6,8,9]
+y2=[1,3,5,7,9]
+plt.plot(x2,y2,'-.')
+plt.title("multiple line graph")
+plt.legned(['Line'])
+plt.xlabel("X-axis")
+plt.ylabel('Y-axis')
+plt.fill_between(x1,y1,y2,color='green',alpha=0.5)
+plt.show() 
