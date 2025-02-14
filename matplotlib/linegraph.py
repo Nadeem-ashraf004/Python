fdad0cf7d0121c01ec5f3321412c906d11c9d8df
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-14 10:44:10 +0500

    advance line graph

--- matplotlib/linegraph.py
+++ matplotlib/linegraph.py
@@ -1,5 +1,6 @@
 ##################### line graph
 import matplotlib.pyplot as plt
+import numpy as np
 x=[1,2,3,4,5,6]
 y=[1,2,3,4]
 plt.plot(x,y)
@@ -34,3 +35,20 @@ plt.ylabel('Y-axis')
 plt.fill_between(x1,y1,y2,color='green',alpha=0.5)
 plt.show()
 ################################# 
+x=np.arange(1,11)
+y1=x**2
+y2=np.log(x)*10
+plt.figure(figsize=(8,5))
+plt.plot(x,y1,color='r',marker='o',linestyle='--',label='y=x^2')
+plt.plot(x,y2,color='r',linestyle='--',marker='o',label='y2=log(x)^2')
+plt.xlabel("X_axis")
+plt.y1label("y1_axis")
+plt.y2label("y2_axis")
+plt.title('Advance Line Graph')
+plt.legend()
+plt.grid(True,linesytle='--',alpha='0.6')
+plt.annotate("Peak",xy=(10,100),xytext=(60,80),arrowprops=dict(facecolor='black',arrowstyle='->'))
+plt.show()
+
+
+        
