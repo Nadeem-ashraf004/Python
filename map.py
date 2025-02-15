45705b0032a0cc74a452ab636c613c50e3153efe
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-15 14:29:45 +0500

    A

--- map.py
+++ map.py
@@ -6,4 +6,11 @@ names=["nadeem","ashraf","hussain"]
 uper=map(str.upper,names)
 print(list(uper))
 ###################
+y=[2,3,4,5,6]
+squar=list(map(lambda x:x**2,y))
+print(squar)
+######################## take list input from user
+number=list(map(int,input("Enter your list :")))
+n=list(map(lambda x:x**2,number))
+print(n)
 
