0786ad5775f15936194ae961d9b5cdb438322e66
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-15 14:16:02 +0500

    uppercase

--- map.py
+++ map.py
@@ -2,4 +2,8 @@ number=[1,2,3,4,5,6]
 square_number=list(map(lambda x:x**2 ,number))
 print(square_number)
 #########################
+names=["nadeem","ashraf","hussain"]
+uper=map(str.upper,names)
+print(list(uper))
+###################
 
