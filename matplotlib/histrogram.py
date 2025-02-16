7646da15044bb5c35192daff9b089b92421bf52e
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-16 15:11:24 +0500

    hist

--- matplotlib/histrogram.py
+++ matplotlib/histrogram.py
@@ -6,3 +6,12 @@ plt.ylabel("y axis")
 plt.title("histogram")
 plt.show()
 #############################
+data1=np.random.randn(1000)
+data2=2*data1+np.random.normal(1000)
+plt.hexbin(data1,data2,gridsize=30,cmap='Blues')
+plt.xlabel('X values')
+plt.ylabel('Y values')
+plt.title('2D Histogram (Hexbin Plot)')
+plt.colorbar()
+plt.show()
+################################
