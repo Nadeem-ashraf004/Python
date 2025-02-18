d6569cc408416ae662d95df56445fa25bccebb53
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-02-18 15:25:52 +0500

    piecahrt in matplotlib

---
+++ matplotlib/piechart.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+cars=['AUDI', 'BMW', 'FORD','TESLA', 'JAGUAR', 'MERCEDES']
+data=[23, 17, 35, 29, 12, 41]
+fig=plt.figure(figsize=(10,7))
+plt.pie(data,labels=cars)
+plt.show()
