fe8e6ddf12f2a0401b81bd4ed64cca307183f59a
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-03-13 20:55:24 +0500

    pivot in python

--- Pandas/pivot.py
+++ Pandas/pivot.py
@@ -6,4 +6,5 @@ df = pd.DataFrame({'Product': ['Carrots', 'Broccoli', 'Banana', 'Banana',
                                 'Vegetable', 'Fruit', 'Vegetable', 'Fruit'],
                    'Quantity': [8, 5, 3, 4, 5, 9, 11, 8],
                    'Amount': [270, 239, 617, 384, 626, 610, 62, 90]})
-df
+print(df)
+###############
