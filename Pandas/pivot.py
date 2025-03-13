b17b1b1eb922e2292da0a3a920602b470ea534fa
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-03-13 20:54:25 +0500

    pivot in python

---
+++ Pandas/pivot.py
@@ -0,0 +1,8 @@
+
+df = pd.DataFrame({'Product': ['Carrots', 'Broccoli', 'Banana', 'Banana',
+                               'Beans', 'Orange', 'Broccoli', 'Banana'],
+                   'Category': ['Vegetable', 'Vegetable', 'Fruit', 'Fruit',
+                                'Vegetable', 'Fruit', 'Vegetable', 'Fruit'],
+                   'Quantity': [8, 5, 3, 4, 5, 9, 11, 8],
+                   'Amount': [270, 239, 617, 384, 626, 610, 62, 90]})
+df
