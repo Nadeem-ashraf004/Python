6d71413d9afe762526e16407667c9bf6c6e55099
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-03-19 07:00:33 +0500

    Cleaning in pandas

---
+++ Pandas/DataCleaning.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+data = {'First Score': [100, 90, np.nan, 95],
+        'Second Score': [30, 45, 56, np.nan],
+        'Third Score': [np.nan, 40, 80, 98]}
+df=pd.DataFrame(data)
+missing_values=df.isnull()
+print("missing values from the dataset !\n",missing_values)
+missing_values=df.notnull()
+print(missing_values)
