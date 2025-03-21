5bf768216f01e6600e38fe8a3b5e0c3a1f6e9ac2
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-03-21 05:49:14 +0500

    removing duplicate in pandas

--- Pandas/RemovDuplicate.py
+++ Pandas/RemovDuplicate.py
@@ -7,4 +7,11 @@ data = {
 df=pd.DataFrame(data)
 unique_df=df.drop_duplicates(subset=['Name'])
 print(unique_df)
-##############
+############## converting datatype 
+data = {'Name': ['John', 'Alice', 'Bob', 'Eve', 'Charlie'], 
+        'Age': [25, 30, 22, 35, 28], 
+        'Gender': ['Male', 'Female', 'Male', 'Female', 'Male'], 
+        'Salary': [50000, 55000, 40000, 70000, 48000]}
+df=pd.DataFrame(data)
+df=df.astype({'Age':'float64','Salary':'str'})
+print(df.dtypes)
