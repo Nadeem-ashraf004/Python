8df72d8a27772860730b23e312da2957693db836
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-05-06 22:05:51 +0500

    m

--- zipfunc.py
+++ zipfunc.py
@@ -3,4 +3,10 @@ ages=(12,12,12,12)
 for name,age  in zip(names,ages):
     print(name,age)
     ##################################
-    
+student=[]
+fatherName=[]
+def amnu():
+      studentDetail=input("Enter students name : ").lower() 
+      fatherNameDetail=input("Enter fathers name :").lower()
+
+
