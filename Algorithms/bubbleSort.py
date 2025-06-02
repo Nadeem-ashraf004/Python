03f4f49fa6bdf9caa461429224ddc4baf1570731
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-06-02 13:59:48 +0500

    m

--- Algorithms/bubbleSort.py
+++ Algorithms/bubbleSort.py
@@ -1,11 +1,17 @@
 def bubble_sort(arr):
+    print(len(arr))
     for n in range(len(arr)-1,0,-1):
         swapped=False
+        print(n)
 #inner loop to compair the adjacent element
         for i in range(n):
+
             if arr[i]>arr[i+1]:
+
                  arr[i],arr[i+1] =arr[i+1],arr[i]
+
                  swapped=True
+
         if not swapped:
             break
 arr=[39, 12, 18, 85, 72, 10, 2, 18]  
---
+++ recursionfunction.py
@@ -0,0 +1,21 @@
+# def show(n):
+#     if(n==0):
+#         return
+#     print(n)
+#     show(n-1)
+# show(5)  
+# # ########################
+# def fact(n):
+#     if(n==1 or n==0):
+#         return 1
+#     else:  
+#       return fact(n-1)*n 
+# print(fact(4))  
+############### print sum of n natural number 
+def calc_sum(n):
+    if (n==0):
+        return 1 
+    print(n)
+    return calc_sum(n-1)+n
+sum=calc_sum(10)
+print(sum)    
